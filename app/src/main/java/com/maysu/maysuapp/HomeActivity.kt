d38d706cd824f2364b41a8e876d2826f4a5b8e9c
CarlosAmayaLaiza <i202415260@cibertec.edu.pe>
2026-02-07 00:08:36 -0500

    Home + navegación inferior + fragments base

--- app/src/main/java/com/maysu/maysuapp/HomeActivity.kt
+++ app/src/main/java/com/maysu/maysuapp/HomeActivity.kt
@@ -1,13 +1,48 @@
 package com.maysu.maysuapp
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.maysu.maysuapp.Fragment.CatalogFragment
+import com.maysu.maysuapp.Fragment.HomeFragment
+import com.maysu.maysuapp.Fragment.OrdersFragment
+import com.maysu.maysuapp.Fragment.SettingsFragment
+import com.maysu.maysuapp.R
 
-class HomeActivity: AppCompatActivity () {
+class HomeActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottomNav = findViewById(R.id.bottomNav)
+
+        // Fragment inicial
+        replaceFragment(HomeFragment())
+
+        bottomNav.setOnItemSelectedListener {
+
+            when(it.itemId) {
+
+                R.id.nav_home -> replaceFragment(HomeFragment())
+
+                R.id.nav_catalog -> replaceFragment(CatalogFragment())
+
+                R.id.nav_orders -> replaceFragment(OrdersFragment())
+
+                R.id.nav_settings -> replaceFragment(SettingsFragment())
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
